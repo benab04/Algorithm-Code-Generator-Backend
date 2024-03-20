1a9f696bdcf6e8622ee9eb059fdf952141f67c15
benab04 <benabrahambiju@gmail.com>
2024-03-20 19:57:26 +0530

    udate allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','https://algorithm-code-generator-backend.onrender.com']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','algorithm-code-generator-backend.onrender.com']
 
 
 # Application definition
